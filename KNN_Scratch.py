93670f02a22ef11ec42f59db0f330612cc30714a
Ganga Lingden <lingden.ganga@gmail.com>
2020-10-14 17:42:53 +0300

    KNN classifier from scratch

--- KNN_Scratch.py
+++ KNN_Scratch.py
@@ -1,9 +1,11 @@
 """"
 Author: Ganga Lingden
-Tested KNN(K-Nearest Neighbours) with two different ways :
-  1. Totally scratch with numpy and python only, and Tested cifar dataset with out data standardized: Results: L1 (38.59%) and L2 (35.39%)
-  2. With scipy package and data is standardized using z-score: l1 (45.46%) and l2 ( 42.26%)
-Dataset: Cifar-10(Python version), Link: https://www.cs.toronto.edu/~kriz/cifar.html
+In this script, KNN(K-Nearest Neighbours) classifier is developed from scratch:
+The script test with the Cifar-10 dataset:
+  Cifar-10(Python version), Link: https://www.cs.toronto.edu/~kriz/cifar.html
+  Results:
+    With out data standardized: L1 (38.59%) and L2 (35.39%)
+    With data standardized: l1 (45.46%) and l2 ( 42.26%)
 """
 
 # Import Packages
@@ -13,6 +15,7 @@ import time
 import glob
 import matplotlib.pyplot as plt
 from random import random
+from scipy import stats
 from scipy.stats import zscore
 from scipy.spatial import distance
 
@@ -55,7 +58,7 @@ def cifar10_classifier_random(x, label):
     --------
     labels (1d array): Predicted labels
     """
-    labels = np.array(range(label))
+    labels = np.array(range(label))  # classes
 
     # Random guess with uniform distribution of each class
     random_labels = np.random.choice(labels, len(x))
@@ -71,135 +74,73 @@ class DistanceMetric:
      y (nd array): vector/matrix
 
     Return:
-     distance (1d array):  Euclidean or Manhattan distance
+     distance (nd array):  Euclidean or Manhattan distance
     """
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    # Euclidean distance
-    def euclidean_distance(self):
+    # Euclidean distance(L2)
+    def euclidean_distance(self, x, y):
 
         # vector to vector(1d to 1d)
-        if self.x.ndim == 1 and self.y.ndim == 1:
-            eud_dist = np.sum((self.x - self.x) ** 2)
-            return eud_dist
+        if x.ndim == 1 and y.ndim == 1:
+            eud_dist = np.sum((x - y) ** 2)
+            return eud_dist  # 1d ouput
 
         # matrix to matrix
-        elif self.x.ndim > 1 and self.y.ndim > 1:
-            if self.x.shape[0] == 1:  # Single data
-                eud_dist = np.sum(((self.x - self.y) ** 2), axis=1)
+        elif x.ndim > 1 and y.ndim > 1:
+            if x.shape[0] == 1:  # Single data
+                eud_dist = np.sum(((x - y) ** 2), axis=1)
                 eud_dist = np.expand_dims(eud_dist, axis=0)  # Change to 2d
-                return eud_dist
+                return eud_dist  # 2d ouput
 
             else:
-                self.x = self.x[:, None]  # Change to 3d
-                eud_dist = np.sum(((self.x - self.y) ** 2), axis=2)
-                return eud_dist
+                x = x[:, None]  # Change to 3d
+                eud_dist = np.sum(((x - y) ** 2), axis=2)
+                return eud_dist  # 2d ouput
 
         # vector to matrix(1d to 2d)
         else:
-            eud_dist = np.sum(((self.x - self.y) ** 2), axis=1)
-            return eud_dist
+            eud_dist = np.sum(((x - y) ** 2), axis=1)
+            return eud_dist  # 1d ouput
 
-    # Manhattan distance
-    def manhattan_dist(self):
+    # Manhattan distance(L1)
+    def manhattan_dist(self, x, y):
         # vector to vector(1d to 1d)
-        if self.x.ndim == 1 and self.y.ndim == 1:
-            man_dist = np.sum(np.abs(self.x - self.y))
-            return man_dist
+        if x.ndim == 1 and y.ndim == 1:
+            man_dist = np.sum(np.abs(x - y))
+            return man_dist  # 1d ouput
 
         # matrix to matrix(2d to 2d)
-        elif self.x.ndim > 1 and self.y.ndim > 1:
-            if self.x.shape[0] == 1:
-                man_dist = np.sum(np.abs(self.x - self.y), axis=1)
+        elif x.ndim > 1 and y.ndim > 1:
+            if x.shape[0] == 1:  # Single data
+                man_dist = np.sum(np.abs(x - y), axis=1)
                 man_dist = np.expand_dims(man_dist, axis=0)  # Change to 2d
-                return man_dist
+                return man_dist  # 2d output
+
             else:
-                self.x = self.x[:, None]  # Change to 3d
-                man_dist = np.sum(np.abs(self.x - self.y), axis=2)
-                return man_dist
+                x = x[:, None]  # Change to 3d
+                print(x)
+                man_dist = np.sum(np.abs(x - y), axis=2)
+
+                return man_dist  # 2d output
 
         # vector to matrix(1d to 2d)
         else:
-            man_dist = np.sum(np.abs(self.x - self.y), axis=1)
-            return man_dist
-
-
-# Define Ecludian distance function
-def euclidean_distance(x, y):
-    """
-    Calculate Euclidean distance
-
-    parameter
-    ---------
-     x (nd array):  vector/matrix
-     y (nd array): vector/matrix
-
-     return:
-      eud_dist (1d array):  Euclidean distance
-
-    """
-
-    # use for vector to vector
-    if x.ndim == 1 and y.ndim == 1:
-        eud_dist = np.sum((x - y) ** 2)
-        return eud_dist
-
-    # matrix to matrix
-    elif x.ndim > 1 and y.ndim > 1:
-        if x.shape[0] == 1:
-            eud_dist = np.sum(((x - y) ** 2), axis=1)
-            eud_dist = np.expand_dims(eud_dist, axis=0)  # Change to 2d
-            return eud_dist
-        else:
-            x = x[:, None]  # Change to 3d
-            eud_dist = np.sum(((x - y) ** 2), axis=2)
-            return eud_dist
-
-    # vector to matrix
-    else:
-        eud_dist = np.sum(((x - y) ** 2), axis=1)
-        return eud_dist
-
-
-# Manhattan distance function
-def manhattan_dist(x, y):
-    # vector to vector(1d to 1d)
-    if x.ndim == 1 and y.ndim == 1:
-        man_dist = np.sum(np.abs(x - y))
-        return man_dist
-
-    # matrix to matrix(2d to 2d)
-    elif x.ndim > 1 and y.ndim > 1:
-        if x.shape[0] == 1:
             man_dist = np.sum(np.abs(x - y), axis=1)
-            man_dist = np.expand_dims(man_dist, axis=0)  # Change to 2d
-            return man_dist
-        else:
-            x = x[:, None]  # Change to 3d
-            man_dist = np.sum(np.abs(x - y), axis=2)
-            return man_dist
-
-    # vector to matrix(1d to 2d)
-    else:
-        man_dist = np.sum(np.abs(x - y), axis=1)
-        return man_dist
+            return man_dist  # 1d ouput
 
 
 # Define K-Nearest Neighbors Classifier
 class KnnClassifier:
     """K-Nearest Neighbors Classifier"""
 
-    def __init__(self, metric, k=1):
+    def __init__(self, metric='euclidean', k=1):
         self.k = k
         self.metric = metric
 
     # Fit data for training
     def fit(self, x_train, y_train):
         self.x_train = x_train
-        self.y_train = x_train
+        self.y_train = y_train
 
     # Prediction
     def prediction(self, x_test):
@@ -208,54 +149,21 @@ class KnnClassifier:
         if x_test.ndim == 1:
             x_test = np.expand_dims(x_test, axis=0)
 
-        dist = self.metric(x_test, self.x_train)  # Distance
-        k_index = np.argsort(dist, axis=1)[:,0:self.k]  # index of k-nearest neighbors
-        k_neighbors = self.y_train[k_index]  # k-nearest neighbors (2d)
+        predicted_label = np.zeros(len(x_test), dtype=int)  # store prediction classes
 
-        predicted_label = np.zeros(len(test), dtype=int)  # store prediction classes
-        for i, neighbour in enumerate(k_neighbors):
-            values, counts = np.unique(neighbour, return_counts=True)  # count each neighbors
-            highest_count_neighbor = values[np.argsort(-counts)][0]  # neighbors with highest count
-            predicted_label[i] = highest_count_neighbor
-        return predicted_label
-'''
-        else:
+        # loop through each test data
+        for i, test in enumerate(x_test):
+            # Distance Metric
+            if self.metric == 'euclidean':
+                dist = DistanceMetric.euclidean_distance(self, test, self.x_train)  # Distance
+            else:
+                dist = DistanceMetric.manhattan_dist(self, test, self.x_train)  # Distance
+
             k_index = np.argsort(dist)[:self.k]  # index of k-nearest neighbors
-            neighbors = tr_label[k_index]  # k-nearest neighbors
-            values, count = np.unique(neighbors, return_counts=True)  # count each neighbors
-            highest_count_neighbor = values[np.argsort(-counts)][0]  # neighbors with highest count
-            predicted_label[i] = highest_count_neighbor
-            return predicted_label
-'''
+            k_neighbors = self.y_train[k_index]  # k-nearest neighbors
+            hig_count_neighbor = stats.mode(k_neighbors).mode  # get neighbors with max count
+            predicted_label[i] = hig_count_neighbor
 
-# Define 1NN classifier function
-def cifar10_classifier_1nn(x, tr_data, tr_label):
-    """
-       1 Nearest Neighbour Classifier
-       Parameters
-       ----------
-       x (ndarray) : Input vector
-       train_data (ndarray): Training dataset of shape (m*d)
-       train_labels (ndarray): Train labels
-
-       Returns
-       -------
-       predicted_label(ndarray) : Predicted lable of Input Vector
-
-       """
-
-    # Input is vector
-    if x.ndim == 1:
-        elu_dist = manhattan_dist(x, tr_data)  # euclidean function
-        sorted_index = np.argsort(elu_dist)[0]  # first index
-        predicted_label = tr_label[sorted_index]  # label with first index
-        return predicted_label
-
-    # Input is matrix
-    else:
-        elu_dist = manhattan_dist(x, tr_data)  # euclidean function
-        sorted_index = np.argsort(elu_dist)[:, 0]  # first index
-        predicted_label = tr_label[sorted_index]  # label with first index
         return predicted_label
 
 
@@ -287,7 +195,7 @@ if __name__ == "__main__":
 
     print('Train data shape:  {}'.format(train_data.shape))
     print('Train Labels shape:  {}\n'.format(train_labels.shape))
-  
+
     # Display random some images
     labeldict = unpickle('../cifar-10-batches/batches.meta')  # change file path
     label_names = labeldict["label_names"]
@@ -308,29 +216,35 @@ if __name__ == "__main__":
     random_labels = cifar10_classifier_random(test_data, 10)
     random_accu = class_acc(test_labels, random_labels)  # accuracy fucntion
     print('Random Classifier Accuracy: {}% \n'.format(random_accu))
-
-    # 1NN classifier
-    print('1NN Classifier Running .......  ')
-    start_time = time.time()  # start time
-    predicted_labels = []  # store predicted lables
-
-    # Loop all test data instances
-    for x in test_data:
-        pred_label = cifar10_classifier_1nn(x, train_data, train_labels)  # call 1NN classifier funtion
-        predicted_labels.append(pred_label)  # append
-
-    accu_1NN = class_acc(test_labels, np.array(predicted_labels))  # accuracy function
-    print("classifier accuracy:  {} %".format(accu_1NN))
-    print("Time taken:  {} seconds".format(round(time.time() - start_time)))
    
-    """"  ====== Using Scipy =======     """
-    # Using Scipy for distance calculation and z-score
+    
+    # Normalize data
+    test_data = test_data / 255.0
+    train_data = train_data / 255.0
+    
+    k = 1 # hyper-parameter for k-NN
+    start_time = time.time() # start time
+    knn = KnnClassifier(k)
+    knn.fit(train_data, train_labels)
+    pred_label = knn.prediction(test_data)
+    accu_kNN = class_acc(test_labels, pred_label)  # accuracy function
+    print("classifier accuracy:  {}%".format(accu_kNN))
+    print("Time taken:  {} seconds".format(round(time.time() - start_time)))
+
+
+    ''' == Using Scipy packages === '''
     test_data = zscore(test_data, axis=1)  # standarized data
     train_data = zscore(train_data, axis=1)
     start_time = time.time()  # start time
+    eudis = distance.cdist(test_data, train_data, 'euclidean')  # Distance
+    k_index = np.argsort(eudis)[:, :k]  # index for k-Nearst Neigbors
+    k_neighbors = train_labels[k_index] # k-nearest neighbors
 
-    eudis = distance.cdist(test_data, train_data, 'euclidean')  # l2 distance
-    index_sorted = np.argsort(eudis)[:, 0]  # pick first closet one
-    accu_1NN = class_acc(test_labels, train_labels[index_sorted])  # accuracy function
-    print('Accuracy:  {}%'.format(accu_1NN))
+    pred_labels = np.zeros(len(test_labels), dtype=np.int16) # store prediction
+    for i, neighbors in enumerate(k_neighbors):
+        hig_count_neig = stats.mode(neighbors).mode  # get neighbors with max count
+        pred_labels[i] = hig_count_neig
+
+    accu_kNN = class_acc(test_labels, pred_labels)  # accuracy
+    print('\nAccuracy:  {}%'.format(accu_kNN))
     print("Time taken:  {} seconds".format(round(time.time() - start_time)))
